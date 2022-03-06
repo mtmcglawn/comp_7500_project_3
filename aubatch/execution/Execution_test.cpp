70864ec8ad5ed09a2388c9c3e946fea29bfa0a1e
Matthew Tyler McGlawn <mtmcglawn@gmail.com>
2022-03-06 09:25:10 -0500

    Add init Execution
    This starts the section of code that will be launched to execute the
    batches.

---
+++ aubatch/execution/Execution_test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "gtest/gtest.h"
+extern "C" {
+#include "Execution.h"
+}
+
+using namespace std;
+
+
+TEST(PWordCount, DoesAssertWork){
+  ASSERT_TRUE(1 == 1);
+}
+
+/*
+struct AUBatchTest : public testing::Test {
+  string expected_output = "Welcome to Tyler's batch job scheduler Version 1.0\nType 'help' to find out more about AUbatch commands.\n";
+  void SetUp() {}
+  void TearDown() {}
+};
+
+TEST_F(AUBatchTest, AUBatchReturnsInt){
+  testing::internal::CaptureStdout();
+  ASSERT_EQ(typeid(aubatch()), typeid(int));
+  std::string output = testing::internal::GetCapturedStdout();
+}
+
+TEST_F(AUBatchTest, AUBatchOutputCorrect){
+  testing::internal::CaptureStdout();
+  aubatch();
+  std::string output = testing::internal::GetCapturedStdout();
+  ASSERT_EQ(output, expected_output);
+}
+*/
+
+
+int main(int argc, char **argv) {
+
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
