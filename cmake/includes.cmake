cd177f24e42c176b3f446356da54e49da6291857
Matthew Tyler McGlawn <mtmcglawn@gmail.com>
2022-03-06 09:28:29 -0500

    Update cmake files
    This update allows for better testing practices when using c as the
    development language. It allows for empty or fake messages to be linked
    to the tests that can produce the same results

--- cmake/includes.cmake
+++ cmake/includes.cmake
@@ -3,8 +3,10 @@ include_guard(GLOBAL)
 #######################################
 # Set up the Include Directories
 #######################################
-set(CMAKE_INC       ${CMAKE_DIR})
-set(AU_BATCH_INC    ${AU_BATCH_DIR})
+set(CMAKE_INC          ${CMAKE_DIR})
+set(AU_BATCH_INC       ${AU_BATCH_DIR})
+set(EXECUTION_INC      ${EXECUTION_DIR})
+set(USER_INTERFACE_INC ${USER_INTERFACE_DIR})
 
 set(COMMON_INCLUDES 
   ${CMAKE_INC}
--- cmake/paths.cmake
+++ cmake/paths.cmake
@@ -3,13 +3,16 @@ include_guard(GLOBAL)
 #######################################
 # Set up the directory locations
 #######################################
-set(TOP_LEVEL_DIR     ${BASE_DIR})
+set(TOP_LEVEL_DIR      ${BASE_DIR})
 
 
-set(BUILD_DIR         ${CMAKE_SOURCE_DIR}/build)
+set(BUILD_DIR          ${CMAKE_SOURCE_DIR}/build)
 
-set(CMAKE_DIR         ${TOP_LEVEL_DIR}/cmake)
-set(EXTERNAL_DEPS_DIR ${TOP_LEVEL_DIR}/external)
-set(AU_BATCH_DIR      ${TOP_LEVEL_DIR}/aubatch)
+set(CMAKE_DIR          ${TOP_LEVEL_DIR}/cmake)
+set(EXTERNAL_DEPS_DIR  ${TOP_LEVEL_DIR}/external)
+set(AU_BATCH_DIR       ${TOP_LEVEL_DIR}/aubatch)
 
-set(GOOGLETEST_DIR    ${EXTERNAL_DEPS_DIR}/googletest)
+set(GOOGLETEST_DIR     ${EXTERNAL_DEPS_DIR}/googletest)
+
+set(EXECUTION_DIR      ${AU_BATCH_DIR}/execution)
+set(USER_INTERFACE_DIR ${AU_BATCH_DIR}/user_interface)
