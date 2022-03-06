8db3546183acc6dab65fa69c885a7a3b4f884f0f
Matthew Tyler McGlawn <mtmcglawn@gmail.com>
2022-03-06 09:26:40 -0500

    Add init user interface
    This starts the section of code that will be launched to interact with
    the user.

---
+++ aubatch/user_interface/UserInterface_fake.c
@@ -0,0 +1,43 @@
+#define _USER_INTERFACE_C
+#include "UserInterface.h"
+
+
+/*
+ * COMP 7500/001
+ * Matthew Tyler McGlawn
+ * Auburn University
+ *
+ * COMPILE
+ *
+ * 2 ways:
+ * 1: $ ./build.sh
+ * 2: $ mkdir build && cd build && cmake .. && cmake --build .
+ *
+ * Either way may be used
+ * 
+ *
+ * TESTS
+ *
+ * To run tests in any directory with source code:
+ * - googletest must be located in repository - run
+ *
+ *   `git submodule update --init`
+ *
+ *    to clone googletest into appropriate location
+ *
+ * First build, then either:
+ * 1. $ cd build && ctest
+ * 2. $ ./build.sh test
+ *
+ *
+ * USE
+ *
+ * To run the program you can either:
+ * 1: $ ./build/pwordcount/pwordcount <file_name>
+ * 2: $ cd ./build/pwordcount/ && ./pwordcount <file_name>
+ */
+
+
+void *launch_user_interface()
+{
+}
